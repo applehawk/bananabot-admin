fbf7ad7cfb1a9b82a4aac38f955d0b683d2d8d50
Vladislav Vasilenko <mr.vasilenko.vlad@ya.ru>
2025-11-24 19:44:58 +0300

    update next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  // Вся админка будет считаться находящейся под /admin
+  basePath: '/admin',
+  assetPrefix: '/admin',
+
+  // Опционально: если используешь SSR и внешние хосты:
+  // experimental: { outputStandalone: true },
 };
 
 export default nextConfig;
