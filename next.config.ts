8a0f4a9a89e29eaa7a14dcfc911a5ca784a154d3
Vladislav Vasilenko <mr.vasilenko.vlad@ya.ru>
2025-11-24 21:35:38 +0300

    update bananabot-admin, added /admin

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  // Вся админка будет считаться находящейся под /admin
+  // basePath для работы admin панели под /admin
   basePath: '/admin',
   assetPrefix: '/admin',
-
-  // Опционально: если используешь SSR и внешние хосты:
-  // experimental: { outputStandalone: true },
 };
 
 export default nextConfig;
